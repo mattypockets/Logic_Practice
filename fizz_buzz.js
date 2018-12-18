dda583428df4caad25e670c0062c1a6bd282918b
Matt Gasper <mrgasper27@gmail.com>
2018-12-18 10:53:32 -0600

    FizzBuzz second attempt

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,7 +1,7 @@
 // First Attempt
 
 let fizzBuzz = () => {
-    for (i=0; i < 100; i++) {
+    for (i=1; i < 100; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz")
         } else if (i % 3 === 0) {
@@ -13,3 +13,19 @@ let fizzBuzz = () => {
 }
 
 fizzBuzz()
+
+// Second Attempt - any range of numbers
+
+let fizzBuzz2 = (n) => {
+    for (i=1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else console.log(i)
+    }
+}
+
+fizzBuzz2(50)
